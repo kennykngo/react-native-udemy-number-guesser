f7a6621f9332f89c853729cd075fd55450bd36e7
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-03-04 12:44:51 -0800

    added React Native's Alert object that calls the alert()

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+  Alert,
   Button,
   Keyboard,
   StyleSheet,
@@ -30,7 +31,12 @@ const StartGameScreen = (props) => {
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
     // only positive integers are allowed
-    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid Number!',
+        'Number has to be a number between 1 and 99',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
       return;
     }
     setConfirmed(true);
